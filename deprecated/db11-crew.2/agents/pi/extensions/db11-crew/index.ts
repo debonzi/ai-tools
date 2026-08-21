ed2cdd418a4198f29e8f66b1f9b9375faabf061c
Daniel Debonzi <debonzi@gmail.com>
2026-08-21 11:43:58 -0300

    cleanup: Remove db11-crew and db11-spec implementations from deprecated folder

--- deprecated/db11-crew.2/agents/pi/extensions/db11-crew/index.ts
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from "node:url";
-
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-import { installCrewleadExtension } from "../../../../src/crewlead/extension.ts";
-
-export default function db11Crew(pi: ExtensionAPI): void {
-  installCrewleadExtension(pi, { extensionPath: fileURLToPath(import.meta.url) });
-}
